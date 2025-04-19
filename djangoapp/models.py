9a7c1312624b53bd6a80bd1c935846b3a0cdc368
HemangaChamod <hemangachamod@gmail.com>
2025-04-19 14:38:45 -0400

    Adding temporary changes to Github

--- djangoapp/models.py
+++ djangoapp/models.py
@@ -23,3 +23,40 @@
 # - Year (IntegerField) with min value 2015 and max value 2023
 # - Any other fields you would like to include in car model
 # - __str__ method to print a car make object
+
+from django.db import models
+from django.utils.timezone import now
+from django.core.validators import MaxValueValidator, MinValueValidator
+
+# Car Make model
+class CarMake(models.Model):
+    name = models.CharField(max_length=100)
+    description = models.TextField()
+    # Optional: add created_at or other custom fields if needed
+    created_at = models.DateTimeField(default=now)
+
+    def __str__(self):
+        return f"{self.name}"
+
+# Car Model model
+class CarModel(models.Model):
+    car_make = models.ForeignKey(CarMake, on_delete=models.CASCADE)  # Many-to-One
+    name = models.CharField(max_length=100)
+    dealer_id = models.IntegerField()  # Dealer ID from external database
+
+    CAR_TYPES = [
+        ('SEDAN', 'Sedan'),
+        ('SUV', 'SUV'),
+        ('WAGON', 'Wagon'),
+        # Add more types if needed
+    ]
+    type = models.CharField(max_length=10, choices=CAR_TYPES, default='SUV')
+    year = models.IntegerField(
+        validators=[
+            MinValueValidator(2015),
+            MaxValueValidator(2023)
+        ]
+    )
+
+    def __str__(self):
+        return f"{self.car_make.name} - {self.name} ({self.year})"
--- djangoapp/views.py
+++ djangoapp/views.py
@@ -16,6 +16,11 @@ from django.contrib.auth import logout
 from django.contrib import messages
 from datetime import datetime
 
+from .models import CarMake, CarModel
+from .populate import initiate
+from django.http import JsonResponse
+
+
 
 from django.http import JsonResponse
 from django.contrib.auth import login, authenticate
@@ -117,3 +122,20 @@ def registration(request):
 # Create a `add_review` view to submit a review
 # def add_review(request):
 # ...
+
+
+
+def get_cars(request):
+    count = CarModel.objects.count()
+    print("CarModel count:", count)
+    if count == 0:
+        initiate()
+    car_models = CarModel.objects.select_related('car_make')
+    cars = []
+    for car_model in car_models:
+        cars.append({
+            "CarModel": car_model.name,
+            "CarMake": car_model.car_make.name
+        })
+    return JsonResponse({"CarModels": cars})
+
