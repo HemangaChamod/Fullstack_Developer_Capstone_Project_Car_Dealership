9beaf90d89e410dced523201fe265bbf841c73d5
HemangaChamod <hemangachamod@gmail.com>
2025-04-21 10:40:57 -0400

    Adding temporary changes to Github

--- djangoapp/views.py
+++ djangoapp/views.py
@@ -19,10 +19,9 @@ from datetime import datetime
 from .models import CarMake, CarModel
 from .populate import initiate
 from django.http import JsonResponse
+from .restapis import get_request, analyze_review_sentiments, post_review
 
 
-
-from django.http import JsonResponse
 from django.contrib.auth import login, authenticate
 import logging
 import json
@@ -139,3 +138,47 @@ def get_cars(request):
         })
     return JsonResponse({"CarModels": cars})
 
+
+# Get all dealerships or dealerships by state
+def get_dealerships(request, state="All"):
+    if state == "All":
+        endpoint = "/fetchDealers"
+    else:
+        endpoint = "/fetchDealers/" + state
+    dealerships = get_request(endpoint)
+    return JsonResponse({"status": 200, "dealers": dealerships})
+
+
+# Get details for a single dealer
+def get_dealer_details(request, dealer_id):
+    if dealer_id:
+        endpoint = "/fetchDealer/" + str(dealer_id)
+        dealership = get_request(endpoint)
+        return JsonResponse({"status": 200, "dealer": dealership})
+    else:
+        return JsonResponse({"status": 400, "message": "Bad Request"})
+
+
+# Get reviews for a dealer and analyze sentiment
+def get_dealer_reviews(request, dealer_id):
+    if dealer_id:
+        endpoint = "/fetchReviews/dealer/" + str(dealer_id)
+        reviews = get_request(endpoint)
+        for review_detail in reviews:
+            response = analyze_review_sentiments(review_detail['review'])
+            print(response)
+            review_detail['sentiment'] = response.get('sentiment', 'neutral')
+        return JsonResponse({"status": 200, "reviews": reviews})
+    else:
+        return JsonResponse({"status": 400, "message": "Bad Request"})
+
+def add_review(request):
+    if request.user.is_anonymous == False:
+        data = json.loads(request.body)
+        try:
+            response = post_review(data)
+            return JsonResponse({"status": 200, "message": "Review submitted successfully"})
+        except:
+            return JsonResponse({"status": 401, "message": "Error in posting review"})
+    else:
+        return JsonResponse({"status": 403, "message": "Unauthorized"})
